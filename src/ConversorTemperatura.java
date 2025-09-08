e567d16baaafd8b08cd3c74a2ee24865add237ad
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2025-09-08 16:05:36 -0300

    mudando o println para print

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -8,7 +8,7 @@ public class ConversorTemperatura {
     public void pedirGrausCelsius(){
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Olá, por favor digite os graus em Celsius:");
+        System.out.print("Olá, por favor digite os graus em Celsius:");
         grausEmCelsius = leitor.nextDouble();
 
         calcularFahrenheit();
